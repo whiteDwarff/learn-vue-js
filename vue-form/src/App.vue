e900ad3a9467aed44d8493adc9b786b90b237b13
whiteDwarf <munstarrrrr@gmail.com>
2023-05-17 21:59:16 +0900

    final commit

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-    <form @submit="submitForm">
+<!-- form 자체의 submit event 제어, button에 이벤트를 등록하지 않는다 !! -->
+<!-- prevent :  event의 기본동작을 제어할 수 있다 -->
+    <form @submit.prevent="submitForm">
       <div>
         <label for="username">id : </label>        
         <!-- v-model : 사용자의 입력값을 data에 저장 -->
+        <!-- 실시간 데이터바인딩  -->
         <input id="username" type="text" v-model="username">
       </div>
       <div>
@@ -14,6 +17,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function() {
     return {
@@ -22,9 +27,22 @@ export default {
     }
   },
   methods: {
-    submitForm: function(e) {
-      e.preventDefault();
+    submitForm: function() {
       console.log(this.username, this.password);
+      // browser에서 server로 데이터를 주고 받기 위한 라이브러리
+      // post : 데이터를 생성 및 저장할 때 생성하는 method
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username : this.username,
+        password : this.password
+      }
+      axios.post(url, data)
+        .then(function(res) {
+            console.log(res);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
       
     }
   }
